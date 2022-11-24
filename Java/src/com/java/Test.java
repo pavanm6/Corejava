a11ba14031c4f50b588a184596ff07d3d4f630d7
pavan <pavan@PAVAN>
2022-11-24 11:55:00 +0530

    my first commit

---
+++ Java/src/com/java/Test.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		for(int i=0; i<5; i++)
+		{
+			for(int j=5; j>i; j--)
+			{
+				System.out.println(" "); //print space
+			}
+			for(int j=0 ; j<i ; j++)
+			{
+				System.out.println("*"); // print star
+			}
+			System.out.println(""); //ending line after each row
+			}
+		}
+	}
